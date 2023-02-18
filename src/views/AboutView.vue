5e185678271eaa1d2453139d4e542279ec72672e
林昱鋐 <yuhunglin@lafresh.com.tw>
2023-02-18 17:35:30 +0800

    vite部屬_final

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
-    {{ user }}
+    <h1>This is an about page4</h1>
+    <!-- <img :src="" alt=""> -->
+    <!-- {{ user.results[0].picture.large }} -->
+    <img
+      :src="user.results[0].picture.large"
+      :alt="user.results[0].picture.large"
+    />
   </div>
 </template>
 
@@ -24,6 +29,7 @@ export default {
     let user = ref();
     axios.get(import.meta.env.VITE_PATH).then((res) => {
       user.value = res.data;
+      console.log(res.data.results[0].picture.large);
     });
     return {
       user,
