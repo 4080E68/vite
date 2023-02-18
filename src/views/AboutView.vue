05085b7faacbe804deb48a9e982bb30ce44c3fa9
林昱鋐 <yuhunglin@lafresh.com.tw>
2023-02-18 16:59:35 +0800

    測試上傳

---
+++ src/views/AboutView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    {{ user }}
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+
+<script>
+import axios from "axios";
+import { ref } from "vue";
+export default {
+  setup(props) {
+    console.log(import.meta.env.VITE_TEXT);
+    let user = ref();
+    axios.get(import.meta.env.VITE_PATH).then((res) => {
+      user.value = res.data;
+    });
+    return {
+      user,
+    };
+  },
+};
+</script>
